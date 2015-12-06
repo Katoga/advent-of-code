0374100352bbba4a9e6b8b01e2993f54567e0c4b
Katoga <katoga.cz@hotmail.com>
2015-12-06 13:11:05 +0100

    Day3, Part1

---
+++ src/Day3/Part1/Solver.php
@@ -0,0 +1,54 @@
+<?php
+namespace AdventOfCode\Day3\Part1;
+
+use AdventOfCode\SolverInterface;
+
+/**
+ *
+ * @author Katoga <katoga.cz@hotmail.com>
+ * @since 2015-12-06
+ * @license https://opensource.org/licenses/ISC ISC licence
+ */
+class Solver implements SolverInterface
+{
+	const STEP_NORTH = '^';
+	const STEP_SOUTH = 'v';
+	const STEP_EAST = '>';
+	const STEP_WEST = '<';
+
+	/**
+	 *
+	 * @param string $input
+	 * @return int
+	 */
+	public function getSolution($input)
+	{
+		// start at 0~0
+		$posX = 0;
+		$posY = 0;
+		$visited = [
+			sprintf('%d~%d', $posX, $posY) => true
+		];
+
+		$steps = str_split($input);
+		foreach ($steps as $step) {
+			switch ($step) {
+				case self::STEP_NORTH:
+					$posY++;
+					break;
+				case self::STEP_SOUTH:
+					$posY--;
+					break;
+				case self::STEP_EAST:
+					$posX++;
+					break;
+				case self::STEP_WEST:
+					$posX--;
+					break;
+			}
+			$visited[sprintf('%d~%d', $posX, $posY)] = true;
+		}
+
+		return count($visited);
+	}
+}
---
+++ tests/Day3/Part1/solver.php
@@ -0,0 +1,19 @@
+<?php
+
+use Tester\Assert;
+
+require_once __DIR__ . '/../../bootstrap.php';
+
+$solver = new AdventOfCode\Day3\Part1\Solver();
+
+$data = [
+	'>' => 2,
+	'^>v<' => 4,
+	'^v^v^v^v^v' => 2,
+	'>>>>>' => 6,
+	'' => 1
+];
+
+foreach ($data as $input => $expected) {
+	Assert::equal($expected, $solver->getSolution($input));
+}
