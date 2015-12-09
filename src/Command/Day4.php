65c946ef4cb090e589aecb8680edec83b0c69783
Katoga <katoga.cz@hotmail.com>
2015-12-09 20:53:44 +0100

    Day4 commands.

---
+++ src/Command/Day4.php
@@ -0,0 +1,15 @@
+<?php
+namespace AdventOfCode\Command;
+
+/**
+ *
+ * @author Katoga <katoga.cz@hotmail.com>
+ * @since 2015-12-09
+ * @license https://opensource.org/licenses/ISC ISC licence
+ */
+abstract class Day4 extends Commons
+{
+	const DESCRIPTION = 'The Ideal Stocking Stuffer';
+
+	const SOLUTION_MESSAGE = 'The Number';
+}
