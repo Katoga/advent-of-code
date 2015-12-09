8d9716322140b1a33baebedd8eaf6d7895ded225
Katoga <katoga.cz@hotmail.com>
2015-12-09 20:43:50 +0100

    Day2 commands.

---
+++ src/Command/Day2.php
@@ -0,0 +1,13 @@
+<?php
+namespace AdventOfCode\Command;
+
+/**
+ *
+ * @author Katoga <katoga.cz@hotmail.com>
+ * @since 2015-12-08
+ * @license https://opensource.org/licenses/ISC ISC licence
+ */
+abstract class Day2 extends Commons
+{
+	const DESCRIPTION = 'I Was Told There Would Be No Math';
+}
---
+++ src/Command/Day2/Part2.php
@@ -0,0 +1,15 @@
+<?php
+namespace AdventOfCode\Command\Day2;
+
+use AdventOfCode\Command\Day2;
+
+/**
+ *
+ * @author Katoga <katoga.cz@hotmail.com>
+ * @since 2015-12-08
+ * @license https://opensource.org/licenses/ISC ISC licence
+ */
+class Part2 extends Day2
+{
+	const SOLUTION_MESSAGE = 'Feet of ribbon';
+}
