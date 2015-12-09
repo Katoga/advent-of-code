bb0f16f7df11828a6364258aabfe963bbba44d91
Katoga <katoga.cz@hotmail.com>
2015-12-09 20:56:49 +0100

    Day5 commands.

---
+++ src/Command/Day5.php
@@ -0,0 +1,15 @@
+<?php
+namespace AdventOfCode\Command;
+
+/**
+ *
+ * @author Katoga <katoga.cz@hotmail.com>
+ * @since 2015-12-09
+ * @license https://opensource.org/licenses/ISC ISC licence
+ */
+abstract class Day5 extends Commons
+{
+	const DESCRIPTION = 'Doesn\'t He Have Intern-Elves For This?';
+
+	const SOLUTION_MESSAGE = 'Number of nice words';
+}
---
+++ src/Command/Day5/Part2.php
@@ -0,0 +1,14 @@
+<?php
+namespace AdventOfCode\Command\Day5;
+
+use AdventOfCode\Command\Day5;
+
+/**
+ *
+ * @author Katoga <katoga.cz@hotmail.com>
+ * @since 2015-12-09
+ * @license https://opensource.org/licenses/ISC ISC licence
+ */
+class Part2 extends Day5
+{
+}
