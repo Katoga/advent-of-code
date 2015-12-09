3dd2d3fe2eac36c8b46b12efe401a83281e970cc
Katoga <katoga.cz@hotmail.com>
2015-12-09 20:47:22 +0100

    Day3 commands.

---
+++ src/Command/Day3/Part1.php
@@ -0,0 +1,14 @@
+<?php
+namespace AdventOfCode\Command\Day3;
+
+use AdventOfCode\Command\Day3;
+
+/**
+ *
+ * @author Katoga <katoga.cz@hotmail.com>
+ * @since 2015-12-09
+ * @license https://opensource.org/licenses/ISC ISC licence
+ */
+class Part1 extends Day3
+{
+}
