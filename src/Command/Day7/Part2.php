47e825cdecf73d066f38cc65e9344629adeea418
Katoga <katoga.cz@hotmail.com>
2015-12-11 19:52:45 +0100

    Day7, Part2

---
+++ src/Command/Day7/Part2.php
@@ -0,0 +1,25 @@
+<?php
+namespace AdventOfCode\Command\Day7;
+
+use AdventOfCode\Command\Day7;
+
+/**
+ *
+ * @author Katoga <katoga.cz@hotmail.com>
+ * @since 2015-12-10
+ * @license https://opensource.org/licenses/ISC ISC licence
+ */
+class Part2 extends Day7
+{
+	const SOLUTION_MESSAGE = 'Signal on wire "%s"';
+
+	/**
+	 *
+	 * {@inheritDoc}
+	 * @see \AdventOfCode\Command\Commons::getSolutionMessage()
+	 */
+	protected function getSolutionMessage()
+	{
+		return sprintf(static::SOLUTION_MESSAGE, $this->solver->getOutputWire());
+	}
+}
