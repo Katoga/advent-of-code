f228eb7830761c3cc1107baccba0dbbc193cabda
Katoga <katoga.cz@hotmail.com>
2015-12-11 21:38:31 +0100

    Common parent for Day4 solvers.

--- src/Lib/Day4/Part1.php
+++ src/Lib/Day4/Part1.php
@@ -1,48 +1,13 @@
 <?php
 namespace AdventOfCode\Lib\Day4;
 
-use AdventOfCode\Lib\SolverInterface;
-
 /**
  *
  * @author Katoga <katoga.cz@hotmail.com>
  * @since 2015-12-06
  * @license https://opensource.org/licenses/ISC ISC licence
  */
-class Part1 implements SolverInterface
+class Part1 extends Common
 {
-
-	/**
-	 *
-	 * @var string
-	 */
-	protected $secretKey;
-
-	/**
-	 *
-	 * @param string $input
-	 * @return int
-	 */
-	public function getSolution($input)
-	{
-		$this->secretKey = $input;
-
-		$number = 1;
-
-		while (!$this->hashStartsWithFiveZeroes($number)) {
-			$number++;
-		}
-
-		return $number;
-	}
-
-	protected function hashStartsWithFiveZeroes($number)
-	{
-		$string = sprintf('%s%d', $this->secretKey, $number);
-		$hash = md5($string);
-
-		$chunk = substr($hash, 0, 5);
-
-		return $chunk === '00000';
-	}
+	const LEADING_ZEROS = 5;
 }
--- src/Lib/Day4/Part2.php
+++ src/Lib/Day4/Part2.php
@@ -1,37 +1,13 @@
 <?php
 namespace AdventOfCode\Lib\Day4;
 
-use AdventOfCode\Lib\SolverInterface;
-
 /**
  *
  * @author Katoga <katoga.cz@hotmail.com>
  * @since 2015-12-06
  * @license https://opensource.org/licenses/ISC ISC licence
  */
-class Part2 implements SolverInterface
+class Part2 extends Common
 {
-
-	/**
-	 *
-	 * @param string $input
-	 * @return int
-	 */
-	public function getSolution($input)
-	{
-			// result from Day4, Part1
-		$number = 282749;
-
-		$string = '';
-
-		do {
-			$number++;
-			$string = substr(md5($input . $number), 0, 6);
-			if ($number % 1000000 == 0) {
-				echo $number . PHP_EOL;
-			}
-		} while ($string !== '000000');
-
-		return $number;
-	}
+	const LEADING_ZEROS = 6;
 }
