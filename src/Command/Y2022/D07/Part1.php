adc97aff547401916f280b7e76d638a3e9999652
Katoga <katoga.cz@hotmail.com>
2022-12-07 14:53:40 +0100

    Solve Y2022 D07 P1

---
+++ src/Command/Y2022/D07/Part1.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AdventOfCode\Command\Y2022\D07;
+
+/**
+ * @author Katoga <katoga.cz@hotmail.com>
+ * @since 2022-12-07
+ * @license https://opensource.org/licenses/ISC ISC licence
+ */
+class Part1 extends Common
+{
+	public const SOLUTION_MESSAGE = 'Sum of sizes of directories';
+}
---
+++ src/Lib/Y2022/D07/Common.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AdventOfCode\Lib\Y2022\D07;
+
+use AdventOfCode\Lib\SolverInterface;
+
+/**
+ * @author Katoga <katoga.cz@hotmail.com>
+ * @since 2022-12-07
+ * @license https://opensource.org/licenses/ISC ISC licence
+ */
+abstract class Common implements SolverInterface
+{
+}
